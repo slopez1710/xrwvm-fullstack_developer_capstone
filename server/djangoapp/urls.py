562eb4141c49542b71054fe84c41a4005d00ead2
Slopez12 <slopez12@students.towson.edu>
2026-07-22 12:42:52 -0400

    changes of mod 4

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -13,12 +13,15 @@ urlpatterns = [
     # path for login
      path(route='login', view=views.login_user, name='login'), 
     # path for dealer reviews view
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
 
     path(route='dealer/<int:dealer_id>/reviews/', view=views.get_dealer_reviews, name='dealer-reviews'),
 
     # path for add a review view
 
-    path(route='dealer/<int:dealer_id>/reviews/add/', view=views.add_review, name='add-review'),
+    path(route='add_review', view=views.add_review, name='add_review'),
     path(route='get_cars', view=views.get_cars, name ='getcars'),
 
 
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -7,6 +7,8 @@ from datetime import datetime
 import logging
 import json
 from .models import CarMake, CarModel
+from .restapis import get_request, analyze_review_sentiments, post_review
+
 
 from .populate import initiate
 
@@ -52,27 +54,50 @@ def registration(request):
         login(request, user)
         return JsonResponse({"userName": username, "status": "User created successfully"})
 
-def get_dealerships(request):
-    if request.method == "GET":
-        dealerships = initiate()
-        return JsonResponse(dealerships, safe=False)
+#Update the `get_dealerships` render list of dealerships all by default, particular state if state is passed
+def get_dealerships(request, state="All"):
+    if(state == "All"):
+        endpoint = "/fetchDealers"
+    else:
+        endpoint = "/fetchDealers/"+state
+    dealerships = get_request(endpoint)
+    return JsonResponse({"status":200,"dealers":dealerships})
+
 
 def get_dealer_reviews(request, dealer_id):
-    if request.method == "GET":
-        reviews = initiate()
-        return JsonResponse(reviews, safe=False)
+    # if dealer id has been provided
+    if(dealer_id):
+        endpoint = "/fetchReviews/dealer/"+str(dealer_id)
+        reviews = get_request(endpoint)
+        for review_detail in reviews:
+            response = analyze_review_sentiments(review_detail['review'])
+            print(response)
+            review_detail['sentiment'] = response['sentiment']
+        return JsonResponse({"status":200,"reviews":reviews})
+    else:
+        return JsonResponse({"status":400,"message":"Bad Request"})
+
 
 def get_dealer_details(request, dealer_id):
-    if request.method == "GET":
-        dealer = initiate()
-        return JsonResponse(dealer, safe=False)
+    if(dealer_id):
+        endpoint = "/fetchDealer/"+str(dealer_id)
+        dealership = get_request(endpoint)
+        return JsonResponse({"status":200,"dealer":dealership})
+    else:
+        return JsonResponse({"status":400,"message":"Bad Request"})
 
 @csrf_exempt
 def add_review(request):
-    if request.method == "POST":
+    if(request.user.is_anonymous == False):
         data = json.loads(request.body)
-        review = initiate()
-        return JsonResponse({"status": "Review submitted successfully"})
+        try:
+            response = post_review(data)
+            return JsonResponse({"status":200})
+        except:
+            return JsonResponse({"status":401,"message":"Error in posting review"})
+    else:
+        return JsonResponse({"status":403,"message":"Unauthorized"})
+
 def get_cars(request):
     count = CarMake.objects.filter().count()
     print(count)
