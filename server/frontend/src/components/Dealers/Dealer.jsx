52c19440f60a061c2042c55e878e2964f08a9b66
Slopez12 <slopez12@students.towson.edu>
2026-07-23 12:45:23 -0400

    mod4

--- server/frontend/src/components/Dealers/Dealer.jsx
+++ server/frontend/src/components/Dealers/Dealer.jsx
@@ -20,9 +20,10 @@ const Dealer = () => {
   let root_url = curr_url.substring(0,curr_url.indexOf("dealer"));
   let params = useParams();
   let id =params.id;
-  let dealer_url = root_url+`djangoapp/dealer/${id}`;
-  let reviews_url = root_url+`djangoapp/reviews/dealer/${id}`;
-  let post_review = root_url+`postreview/${id}`;
+  let dealer_url = `/djangoapp/dealer/${id}`;
+  let reviews_url = `/djangoapp/reviews/dealer/${id}`;
+  let post_review = `/postreview/${id}`;
+
   
   const get_dealer = async ()=>{
     const res = await fetch(dealer_url, {
--- server/frontend/src/components/Dealers/Dealers.jsx
+++ server/frontend/src/components/Dealers/Dealers.jsx
@@ -15,16 +15,14 @@ const Dealers = () => {
   let dealer_url_by_state = "/djangoapp/get_dealers/";
  
   const filterDealers = async (state) => {
-    dealer_url_by_state = dealer_url_by_state+state;
-    const res = await fetch(dealer_url_by_state, {
-      method: "GET"
-    });
-    const retobj = await res.json();
-    if(retobj.status === 200) {
-      let state_dealers = Array.from(retobj.dealers)
-      setDealersList(state_dealers)
-    }
+  const url = `/djangoapp/get_dealers/${state}`;
+  const res = await fetch(url);
+  const retobj = await res.json();
+
+  if (retobj.status === 200) {
+    setDealersList(Array.from(retobj.dealers || []));
   }
+};
 
   const get_dealers = async ()=>{
     const res = await fetch(dealer_url, {
